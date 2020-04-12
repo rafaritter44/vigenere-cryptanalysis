07c84acedc3e44608071b7363f5d8658cf6f4188
Rafael Ritter <rafaritter44@hotmail.com>
2020-04-12 15:32:59 -0300

    Implement Frequency Analysis

---
+++ src/main/java/com/github/rafaritter44/security/vigenere/cryptanalysis/DifferentAlphabetSizesException.java
@@ -0,0 +1,11 @@
+package com.github.rafaritter44.security.vigenere.cryptanalysis;
+
+public class DifferentAlphabetSizesException extends RuntimeException {
+	
+	private static final long serialVersionUID = 8916246857057772851L;
+	
+	public DifferentAlphabetSizesException(final int sizeA, final int sizeB) {
+		super(String.format("The alphabet sizes are different: %d and %d", sizeA, sizeB));
+	}
+
+}
---
+++ src/main/java/com/github/rafaritter44/security/vigenere/cryptanalysis/FrequencyAnalyzer.java
@@ -0,0 +1,59 @@
+package com.github.rafaritter44.security.vigenere.cryptanalysis;
+
+import static java.util.Map.Entry.comparingByValue;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collector.of;
+import static java.util.stream.Collectors.toConcurrentMap;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class FrequencyAnalyzer {
+	
+	private final Map<Character, Double> letterFrequencies;
+	
+	public FrequencyAnalyzer(final Map<Character, Double> letterFrequencies) {
+		this.letterFrequencies = letterFrequencies
+				.entrySet()
+				.parallelStream()
+				.sorted(comparingByValue())
+				.collect(toConcurrentMap(Entry::getKey, Entry::getValue));
+	}
+	
+	public String decrypt(final String ciphertext) {
+		if (ciphertext == null || ciphertext.isEmpty()) {
+			return "";
+		}
+		final double ciphertextLength = (double) ciphertext.length();
+		final Map<Character, Double> letterFrequencies = ciphertext
+				.chars()
+				.parallel()
+				.mapToObj(letter -> (char) letter)
+				.collect(toConcurrentMap(identity(), letter -> 1D, Double::sum))
+				.entrySet()
+				.parallelStream()
+				.map(kv -> {
+					kv.setValue(kv.getValue() / ciphertextLength);
+					return kv;
+				})
+				.sorted(comparingByValue())
+				.collect(toConcurrentMap(Entry::getKey, Entry::getValue));
+		if (letterFrequencies.size() != this.letterFrequencies.size()) {
+			throw new DifferentAlphabetSizesException(letterFrequencies.size(), this.letterFrequencies.size());
+		}
+		final Map<Character, Character> ciphertextToPlaintext = new ConcurrentHashMap<>();
+		final Iterator<Character> encryptedLetters = letterFrequencies.keySet().iterator();
+		for (final Character plaintextLetter : this.letterFrequencies.keySet()) {
+			ciphertextToPlaintext.put(encryptedLetters.next(), plaintextLetter);
+		}
+		return ciphertext
+				.chars()
+				.parallel()
+				.mapToObj(letter -> (char) letter)
+				.map(ciphertextToPlaintext::get)
+				.collect(of(StringBuffer::new, StringBuffer::append, StringBuffer::append, StringBuffer::toString));
+	}
+	
+}
