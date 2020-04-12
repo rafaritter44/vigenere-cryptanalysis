25fca4803191fc813008b3c21110a0e6d22d5a39
Rafael Ritter <rafaritter44@hotmail.com>
2020-04-12 15:30:53 -0300

    Implement mergeSplitPlaintext method

--- src/main/java/com/github/rafaritter44/security/vigenere/cryptanalysis/CiphertextSplitter.java
+++ src/main/java/com/github/rafaritter44/security/vigenere/cryptanalysis/CiphertextSplitter.java
@@ -1,6 +1,7 @@
 package com.github.rafaritter44.security.vigenere.cryptanalysis;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CiphertextSplitter {
 	
@@ -12,13 +13,33 @@ public class CiphertextSplitter {
 		for (int i = 0; i < keyLength; i++) {
 			splitCiphertext.add("");
 		}
-		final int cipertextLength = ciphertext.length();
-		for (int letterIndex = 0; letterIndex < cipertextLength; letterIndex++) {
+		final int ciphertextLength = ciphertext.length();
+		for (int letterIndex = 0; letterIndex < ciphertextLength; letterIndex++) {
 			final char letter = ciphertext.charAt(letterIndex);
 			final int splitCiphertextIndex = letterIndex % keyLength;
 			splitCiphertext.set(splitCiphertextIndex, splitCiphertext.get(splitCiphertextIndex) + letter);
 		}
 		return splitCiphertext;
+	}
+	
+	public String mergeSplitPlaintext(final List<String> splitPlaintext) {
+		if (splitPlaintext == null || splitPlaintext.isEmpty() || splitPlaintext.contains(null)) {
+			return "";
+		}
+		final StringBuilder mergedPlaintext = new StringBuilder();
+		final int keyLength = splitPlaintext.size();
+		boolean over = false;
+		for (int letterIndex = 0; !over; letterIndex++) {
+			for (int splitPlaintextIndex = 0; splitPlaintextIndex < keyLength; splitPlaintextIndex++) {
+				final String chunk = splitPlaintext.get(splitPlaintextIndex);
+				if (letterIndex >= chunk.length()) {
+					over = true;
+					break;
+				}
+				mergedPlaintext.append(chunk.charAt(letterIndex));
+			}
+		}
+		return mergedPlaintext.toString();
 	}
 	
 }
