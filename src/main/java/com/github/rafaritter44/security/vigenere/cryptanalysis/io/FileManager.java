c4ad6fef460d01ec6bd5874b5a7a849f01366c64
Rafael Ritter <rafaritter44@hotmail.com>
2020-04-14 00:51:06 -0300

    Change imports to static

--- src/main/java/com/github/rafaritter44/security/vigenere/cryptanalysis/io/FileManager.java
+++ src/main/java/com/github/rafaritter44/security/vigenere/cryptanalysis/io/FileManager.java
@@ -1,6 +1,9 @@
 package com.github.rafaritter44.security.vigenere.cryptanalysis.io;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.Paths.get;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -9,14 +12,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Optional;
 
 public class FileManager {
 	
 	public Optional<String> read(final String file) {
 		if (file == null || file.isEmpty()) {
-			return Optional.empty();
+			return empty();
 		}
 		try (final InputStream inputStream = new FileInputStream(new File(file))) {
 			final ByteArrayOutputStream result = new ByteArrayOutputStream();
@@ -25,9 +27,9 @@ public class FileManager {
 			while ((length = inputStream.read(buffer)) != -1) {
 				result.write(buffer, 0, length);
 			}
-			return Optional.of(result.toString(UTF_8.name()).trim());
+			return of(result.toString(UTF_8.name()).trim());
 		} catch (final FileNotFoundException e) {
-			return Optional.empty();
+			return empty();
 		} catch (final IOException e) {
 			throw new RuntimeException(e.getMessage(), e);
 		}
@@ -35,7 +37,7 @@ public class FileManager {
 	
 	public void write(final String file, final String text) {
 		try {
-			Files.write(Paths.get(file), text.getBytes(UTF_8));
+			Files.write(get(file), text.getBytes(UTF_8));
 		} catch (final IOException e) {
 			throw new RuntimeException(e.getMessage(), e);
 		}
